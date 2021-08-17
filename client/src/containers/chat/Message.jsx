f8041ee166e36c25841a081870a56e715b60a174
ByronM_tutenlabs <byron.miranda@tutenlabs.com>
2021-08-16 22:55:44 -0400

    dialog para agregar contactos y visualizarlos

--- client/src/containers/chat/Message.jsx
+++ client/src/containers/chat/Message.jsx
@@ -1,22 +1,24 @@
-import "./message.css"
-import userIcon from "../../assets/images/userIcon.png"
-import fileMessageIcon from "../../assets/images/fileMessageIcon.png"
+import './message.css';
+import userIcon from '../../assets/images/userIcon.png';
+import fileMessageIcon from '../../assets/images/fileMessageIcon.png';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
-import {format} from 'timeago.js'
+import { format } from 'timeago.js';
 
-export default function Message({message ,own, file}) {
-    return (
-        <div className={own ? "message own" : "message"}>
-            <div className="messageTop">
-                <img className={!own && "messageImg"} src={!own && userIcon} alt="" />
-                <div className="messageBubble">
-                    <img className={file && "fileMessageImage"} src={file && fileMessageIcon} alt="" />
-                    <span className="messageText">
-                        {message.text}
-                    </span>
-                </div>
-            </div>
-            <div className="messageBottom">{format(message.createdAt)}</div>
+export default function Message({ message, own, file }) {
+  return (
+    <div className={own ? 'message own' : 'message'}>
+      <div className='messageTop'>
+        <img className={!own && 'messageImg'} src={!own && userIcon} alt='' />
+        <div className='messageBubble'>
+          <img
+            className={file && 'fileMessageImage'}
+            src={file && fileMessageIcon}
+            alt=''
+          />
+          <span className='messageText'>{message.text}</span>
         </div>
-    )
+      </div>
+      <div className='messageBottom'>{format(message.createdAt)}</div>
+    </div>
+  );
 }
