dbc589783da2f48e86831cc212f30681a1b704ee
AndrewSuarez <andresesuareze@gmail.com>
2021-08-17 20:13:51 -0400

    chat grupal

--- client/src/containers/chat/Message.jsx
+++ client/src/containers/chat/Message.jsx
@@ -1,12 +1,30 @@
 import './message.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import userIcon from '../../assets/images/userIcon.png';
 import fileMessageIcon from '../../assets/images/fileMessageIcon.png';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import { format } from 'timeago.js';
 
 export default function Message({ message, own, file }) {
+
+  const [sender, setSender] = useState([])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get('/api/users/' + message.sender);
+        setSender(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchUser();
+  }, [])
+
   return (
     <div className={own ? 'message own' : 'message'}>
+      {!own && <div className="senderName">{sender.nombre}</div> }
       <div className='messageTop'>
         <img className={!own && 'messageImg'} src={!own && userIcon} alt='' />
         <div className='messageBubble'>
--- routes/api/conversation.js
+++ routes/api/conversation.js
@@ -15,14 +15,36 @@ router.post('/:groupId', async(req, res)=>{
     }
 })
 
-//get conversacion
-router.get('/:groupId/:userId/:memberId', async(req, res)=> {
+//Nuevo conversacion Grupal
+router.post('/groupchat/:groupId', async(req, res)=>{
+    const newConversation = new Conversation({
+        groupId: req.params.groupId,
+    })
     try{
-        const conversation = await Conversation.findOne({
-            groupId: req.params.groupId,
-            members: {$all: [req.params.userId, req.params.memberId]},
-        });
-        res.status(200).json(conversation)
+        const savedConversation = await newConversation.save();
+        res.status(200).json(savedConversation);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+//get conversacion
+router.get('/:groupId/:userId?/:memberId?', async(req, res)=> {
+    try{
+        if ((req.params.userId) && (req.params.memberId)){
+            const conversation = await Conversation.findOne({
+                groupId: req.params.groupId,
+                members: {$all: [req.params.userId, req.params.memberId]},
+            });
+            res.status(200).json(conversation)
+        }else{
+            const conversation = await Conversation.findOne({
+                groupId: req.params.groupId,
+                members: { $exists: true, $size: 0 },
+            });
+            res.status(200).json(conversation)
+        }
     }catch(err){
         res.status(500).json(err);
     }
