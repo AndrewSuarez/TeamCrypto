73b7e5b398194db0a7ce66120d856d1d362105a9
ByronM_tutenlabs <byron.miranda@tutenlabs.com>
2021-08-17 22:55:02 -0400

    feature: filtrado de usuarios en addContacts

--- client/src/components/ContactsDialog/AddContactDialog/index.js
+++ client/src/components/ContactsDialog/AddContactDialog/index.js
@@ -4,14 +4,23 @@ import Modal from '../../modal';
 // import useStyles from './styles';
 import ElementsList from '../../ElementsList';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
 import { Radio, TextField } from '@material-ui/core';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
+import Search from '@material-ui/icons/Search';
 
 const AddContactsDialog = ({ open, handleClose }) => {
-  //   const classes = useStyles();
+  const classes = useStyles();
 
   const [userAvatar, setUserAvatar] = useState([]);
-  const [users, setUsers] = useState([
+  const [keyword, setKeyword] = useState('');
+  const [users, setUsers] = useState([]);
+  const [selectedValue, setSelectedValue] = useState([]);
+  const [originalUsers, setOriginalUsers] = useState([
     {
       avatar: userAvatar[0],
       title: 'Andres Suarez',
@@ -29,23 +38,36 @@ const AddContactsDialog = ({ open, handleClose }) => {
       title: 'Eduardo Leon',
     },
   ]);
-  const [firtsUsers, setFirstUsers] = useState(users);
 
   useEffect(() => {
     fetch('https://picsum.photos/v2/list?page=2&limit=4').then((res) => {
       return res.json().then((data) => {
         // console.log(data);
         setUserAvatar(data);
+        setUsers(originalUsers);
       });
     });
   }, []);
 
-  const handleSearch = (e) => {
-    const key = e.target.value;
+  const selectItems = (e) => {
+    setSelectedValue(e.target.value);
+    console.log(e.target);
+  };
+
+  const handleSearch = (key) => {
     const listFilter = users.filter((i) => {
       return i.title.includes(key);
     });
     setUsers(listFilter);
+  };
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const key = e.target.value;
+    if (key == '') {
+      setUsers(originalUsers);
+    }
+    setKeyword(key);
   };
 
   return (
@@ -57,15 +79,31 @@ const AddContactsDialog = ({ open, handleClose }) => {
       handleClose={handleClose}
       maxWidth='xs'
     >
-      <form noValidate autoComplete='off'>
-        <TextField
+      <form noValidate autoComplete='off' className={classes.root}>
+        <Input
           required
-          id='standard-required'
-          label='Required'
-          placeholder='Buscar un contacto por su nombre'
-          onChange={handleSearch}
+          placeholder='Buscar'
+          className={classes.search}
+          onChange={handleFilter}
+          endAdornment={
+            <InputAdornment className={classes.searchIcon}>
+              <IconButton onClick={() => handleSearch(keyword)}>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
         />
-        <ElementsList items={users} controlItem={<Radio />} />
+        <ElementsList
+          items={users}
+          controlItem={
+            <Radio
+              onClick={selectItems}
+              checked={users.filter((user) => {
+                return user == selectedValue;
+              })}
+            />
+          }
+        />
       </form>
     </Modal>
   );
