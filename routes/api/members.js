bc58a812b567dc0f395f6012bf9d6780e63850c5
ByronM_tutenlabs <byron.miranda@tutenlabs.com>
2021-08-23 09:58:23 -0400

    Se modifico el schema de members para aprovechar mas las relaciones

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,130 +1,143 @@
 const router = require('express').Router();
 const Member = require('../../models/Member');
 const User = require('../../models/User');
-const Group = require('../../models/Group')
+const Group = require('../../models/Group');
 
 //Nuevo miembro
-router.post('/', async (req, res) =>{
-    const newMember = new Member(req.body)
-    try{
-        const savedMember = await newMember.save();
-        res.status(200).json(savedMember);
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.post('/', async (req, res) => {
+  const newMember = new Member(req.body);
+  try {
+    const savedMember = await newMember.save();
+    res.status(200).json(savedMember);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get all members for a group
+router.get('/prueba/:groupId/:except?', function (req, res) {
+  Member.find({ groupId: req.params.groupId }, (err, member) => {
+    User.populate(member, { path: 'userId' }, function (err, member) {
+      const members = member.filter((m) => {
+        return m.userId._id != req.params.except;
+      });
+      res.status(200).send(members);
+    });
+  });
+});
 
 //Get
-router.get('/:groupId/:userId?', async(req, res)=>{
-    try{
-        const allMembers = await Member.find({
-            groupId: req.params.groupId
-        })
-        const members = allMembers.filter((member) => {
-            return member.userId !== req.params.userId
-        })
-        res.status(200).json(members)
-        }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get('/:groupId/:userId?', async (req, res) => {
+  try {
+    const allMembers = await Member.find({
+      groupId: req.params.groupId,
+    });
+    const members = allMembers.filter((member) => {
+      return member.userId !== req.params.userId;
+    });
+    res.status(200).json(members);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get current group user
-router.get('/group/user/:groupId/:userId', async(req, res) => {
-    try{
-        const member = await Member.findOne({
-            groupId: req.params.groupId,
-            userId: req.params.userId
-        })
-        res.status(200).json(member)
-    }catch(err){
-        res.status(500).json(err)
-    }
-}) 
-
+router.get('/group/user/:groupId/:userId', async (req, res) => {
+  try {
+    const member = await Member.findOne({
+      groupId: req.params.groupId,
+      userId: req.params.userId,
+    });
+    res.status(200).json(member);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Todos los grupos de un usuario
-router.get('/groups/user/:userId', async(req, res) => {
-    try{
-        const currentUser = await User.findById(req.params.userId)
-        const memberGroups = await Member.find({userId: currentUser._id});
-        // const {groupId} = memberGroups._doc
-        const userGroups = await Promise.all(
-            memberGroups.map((members) => {
-                return Group.findById(members.groupId)
-            })
-        )
-        res.status(200).json(userGroups)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get('/groups/user/:userId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId);
+    const memberGroups = await Member.find({ userId: currentUser._id });
+    // const {groupId} = memberGroups._doc
+    const userGroups = await Promise.all(
+      memberGroups.map((members) => {
+        return Group.findById(members.groupId);
+      })
+    );
+    res.status(200).json(userGroups);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Update
-router.put('/:groupId', async(req,res)=> {
-    if(req.body.groupId === req.params.groupId){
-            try{
-                if(req.body.role){
-                    const members = await Member.findOneAndUpdate({
-                        groupId: req.body.groupId,
-                        userId: req.body.userId
-                    }, 
-                        {role: req.body.role,}
-                    )
-                }else if(req.body.tareas){
-                    const members = await Member.findOneAndUpdate({
-                        groupId: req.body.groupId,
-                        userId: req.body.userId
-                    }, 
-                        {$push: {tareas: req.body.tareas}}
-                    )
-                }
-                res.status(200).json('Cuenta actualizada')
-            }catch(err){
-                return res.status(500).json(err)
-            }
-    }else {
-        return res.status(403).json('Solo puedes modificar tu grupo')
+router.put('/:groupId', async (req, res) => {
+  if (req.body.groupId === req.params.groupId) {
+    try {
+      if (req.body.role) {
+        const members = await Member.findOneAndUpdate(
+          {
+            groupId: req.body.groupId,
+            userId: req.body.userId,
+          },
+          { role: req.body.role }
+        );
+      } else if (req.body.tareas) {
+        const members = await Member.findOneAndUpdate(
+          {
+            groupId: req.body.groupId,
+            userId: req.body.userId,
+          },
+          { $push: { tareas: req.body.tareas } }
+        );
+      }
+      res.status(200).json('Cuenta actualizada');
+    } catch (err) {
+      return res.status(500).json(err);
     }
+  } else {
+    return res.status(403).json('Solo puedes modificar tu grupo');
+  }
 });
 
 //borrar tareas
-router.put('/:groupId/tarea', async(req,res)=> {
-    if(req.body.groupId === req.params.groupId){
-            try{
-                const members = await Member.findOne({
-                    groupId: req.body.groupId,
-                    userId: req.body.userId
-                })
-                if (members.tareas.includes(req.body.tareas)){
-                    await members.updateOne(({$pull: {tareas:req.body.tareas}}))
-                    res.status(200).json('Tarea Eliminada')
-                }else{
-                    res.status(404).json('Este usuario no tiene esta tarea pendiente');
-                }
-            }catch(err){
-                res.status(500).json(err)
-            }
-    }else {
-        return res.status(403).json('Solo puedes modificar tu grupo')
+router.put('/:groupId/tarea', async (req, res) => {
+  if (req.body.groupId === req.params.groupId) {
+    try {
+      const members = await Member.findOne({
+        groupId: req.body.groupId,
+        userId: req.body.userId,
+      });
+      if (members.tareas.includes(req.body.tareas)) {
+        await members.updateOne({ $pull: { tareas: req.body.tareas } });
+        res.status(200).json('Tarea Eliminada');
+      } else {
+        res.status(404).json('Este usuario no tiene esta tarea pendiente');
+      }
+    } catch (err) {
+      res.status(500).json(err);
     }
+  } else {
+    return res.status(403).json('Solo puedes modificar tu grupo');
+  }
 });
 
 //Borrar miembro
-router.delete('/:groupId', async(req,res)=> {
-    if(req.body.groupId === req.params.groupId){
-        try{
-            const members = await Member.findOneAndDelete({
-                groupId: req.body.groupId,
-                userId: req.body.userId
-            })
-            res.status(200).json("Miembro eliminado")
-        }catch(err){
-            res.status(500).json(err)
-        }
-    }else {
-        return res.status(403).json('Solo puedes borrar miembros de tu grupo')
+router.delete('/:groupId', async (req, res) => {
+  if (req.body.groupId === req.params.groupId) {
+    try {
+      const members = await Member.findOneAndDelete({
+        groupId: req.body.groupId,
+        userId: req.body.userId,
+      });
+      res.status(200).json('Miembro eliminado');
+    } catch (err) {
+      res.status(500).json(err);
     }
+  } else {
+    return res.status(403).json('Solo puedes borrar miembros de tu grupo');
+  }
 });
 
-module.exports = router 
+module.exports = router;
