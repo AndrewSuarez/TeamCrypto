9e6efc9569df99b25197e0e2d7edd311e50e5162
ByronM_tutenlabs <byron.miranda@tutenlabs.com>
2021-08-28 23:27:29 -0400

    feature: mini menu para grupos y disble al boton aceptar en crear grupos

--- client/src/containers/chat/Group.jsx
+++ client/src/containers/chat/Group.jsx
@@ -1,15 +1,37 @@
-import "./group.css"
-import reactLogo from "../../assets/images/reactLogo.png"
+import React, { useState } from 'react';
+import './group.css';
+import reactLogo from '../../assets/images/reactLogo.png';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-export default function Group({group}) {
-    return (
-        <div className="group">
-            <img className="groupImg" src={reactLogo} alt="" />
-            <span className="groupName">{group?.name}</span>
-            <div className="groupSettingsIcon">
-                <MoreVertIcon  />
-            </div>
-        </div>
-    )
+export default function Group({ group }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div className='group'>
+      <img className='groupImg' src={reactLogo} alt='' />
+      <span className='groupName'>{group?.name}</span>
+      <div className='groupSettingsIcon'>
+        <MoreVertIcon onClick={handleClick} />
+        <Menu
+          id='simple-menu'
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Editar Grupo</MenuItem>
+          <MenuItem onClick={handleClose}>Borrar Grupo</MenuItem>
+        </Menu>
+      </div>
+    </div>
+  );
 }
